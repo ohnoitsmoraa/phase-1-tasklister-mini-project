7aa1ab9d724897a6a9082700fd0d90162dabd5b2
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-08 12:39:57 +0300

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.querySelector("form").addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  toDoList(ev.target.new_task.value);
+});
+
+function toDoList(todoItems) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", toDelete);
+  btn.textContent = "delete";
+  p.textContent = `${todoItems}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+
+function toDelete(ev) {
+  ev.target.parentNode.remove();
+}
